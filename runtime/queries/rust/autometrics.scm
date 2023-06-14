59226d25e4f5d57e1f197ad792872a2d6ee9a578
Gerry Agbobada <gerry@fiberplane.com>
2023-06-14 10:22:47 +0200

    Add 1-level module detection to queries

--- runtime/queries/rust/autometrics.scm
+++ runtime/queries/rust/autometrics.scm
@@ -1,5 +1,4 @@
-(
- (attribute_item)*
+((attribute_item)*
  .
  (attribute_item
    (attribute
@@ -9,11 +8,10 @@
  .
  (function_item
    name: (identifier) @func.name)
- (#eq? @attr "autometrics")
-)
+ (#eq? @attr "autometrics"))
 
-(
- (attribute_item)*
+
+((attribute_item)*
  .
  (attribute_item
    (attribute
@@ -31,11 +29,10 @@
              name: (identifier) @func.name)))
 
  (#eq? @attr "autometrics")
- (#eq? @type.impl @type.target)
-)
+ (#eq? @type.impl @type.target))
 
-(
- (impl_item
+
+((impl_item
    type: (type_identifier) @type.impl
    body: (declaration_list
            (function_item
@@ -53,11 +50,10 @@
    name: (type_identifier) @type.target)
 
  (#eq? @attr "autometrics")
- (#eq? @type.impl @type.target)
-)
+ (#eq? @type.impl @type.target))
 
-(
- (attribute_item)*
+
+((attribute_item)*
  .
  (attribute_item
    (attribute
@@ -68,11 +64,10 @@
  (struct_item
    name: (type_identifier) @type.target)
 
- (#eq? @attr "autometrics")
-)
+ (#eq? @attr "autometrics"))
 
-(
- (attribute_item)*
+
+((attribute_item)*
  .
  (attribute_item
    (attribute
@@ -86,6 +81,112 @@
            (function_item
              name: (identifier) @func.name)))
 
- (#eq? @attr "autometrics")
-)
+ (#eq? @attr "autometrics"))
 
+;; It is impossible to do arbitrary levels of nesting, so we just try 1 module deep
+;; Ref: https://github.com/tree-sitter/tree-sitter/discussions/981
+(
+ (mod_item
+  name: (identifier) @mod.name
+  body: (declaration_list
+         (attribute_item)*
+         .
+         (attribute_item
+                  (attribute
+                           (identifier) @attr))
+         .
+         (attribute_item)*
+         .
+         (function_item
+                  name: (identifier) @func.name)))
+ (#eq? @attr "autometrics"))
+
+
+(
+ (mod_item
+  name: (identifier) @mod.name
+  body: (declaration_list
+
+         (attribute_item)*
+         .
+         (attribute_item
+          (attribute
+           (identifier) @attr))
+         .
+         (attribute_item)*
+         .
+         (struct_item
+          name: (type_identifier) @type.target)
+
+         (impl_item
+          type: (type_identifier) @type.impl
+          body: (declaration_list
+                 (function_item
+                  name: (identifier) @func.name)))))
+
+ (#eq? @attr "autometrics")
+ (#eq? @type.impl @type.target))
+
+
+(
+ (mod_item
+  name: (identifier) @mod.name
+  body: (declaration_list
+         (impl_item
+          type: (type_identifier) @type.impl
+          body: (declaration_list
+                 (function_item
+                  name: (identifier) @func.name)))
+         (attribute_item)*
+         .
+         (attribute_item
+          (attribute
+           (identifier) @attr))
+         .
+         (attribute_item)*
+         .
+         (struct_item
+          name: (type_identifier) @type.target)))
+
+ (#eq? @attr "autometrics")
+ (#eq? @type.impl @type.target))
+
+
+(
+ (mod_item
+  name: (identifier) @mod.name
+  body: (declaration_list
+         (attribute_item)*
+         .
+         (attribute_item
+          (attribute
+           (identifier) @attr))
+         .
+         (attribute_item)*
+         .
+         (struct_item
+          name: (type_identifier) @type.target)))
+
+ (#eq? @attr "autometrics"))
+
+
+(
+ (mod_item
+  name: (identifier) @mod.name
+  body: (declaration_list
+
+         (attribute_item)*
+         .
+         (attribute_item
+          (attribute
+           (identifier) @attr))
+         .
+         (attribute_item)*
+         .
+         (impl_item
+          type: (type_identifier) @type.impl
+          body: (declaration_list
+                 (function_item
+                  name: (identifier) @func.name)))))
+
+ (#eq? @attr "autometrics"))
