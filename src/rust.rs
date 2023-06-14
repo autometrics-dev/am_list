b49cef7017a071ccaa46bebc148941886ed11772
Gerry Agbobada <gerry@fiberplane.com>
2023-06-14 15:48:52 +0200

    fix(rust): nested `mod` detection and put struct name in module

--- src/rust.rs
+++ src/rust.rs
@@ -1,4 +1,5 @@
 use crate::{AmlError, ExpectedAmLabel, ListAmFunctions, Result, FUNC_NAME_CAPTURE};
+use log::trace;
 use rayon::prelude::*;
 use std::{
     collections::{HashSet, VecDeque},
@@ -6,11 +7,13 @@ use std::{
     fs::read_to_string,
     path::Path,
 };
-use tree_sitter::{Parser, Query};
+use tree_sitter::{Node, Parser, Query};
 use tree_sitter_rust::language;
 use walkdir::{DirEntry, WalkDir};
 
 const STRUCT_NAME_CAPTURE: &str = "type.target";
+const MOD_NAME_CAPTURE: &str = "mod.name";
+const MOD_CONTENTS_CAPTURE: &str = "mod.contents";
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct AmStruct {
@@ -125,16 +128,9 @@ impl ListAmFunctions for Impl {
                 .par_iter()
                 .filter_map(move |(path, module)| {
                     let source = read_to_string(path).ok()?;
-                    let names = list_function_names(&source).unwrap_or_default();
-                    Some(
-                        names
-                            .into_iter()
-                            .map(move |fn_name| ExpectedAmLabel {
-                                module: module.clone(),
-                                function: fn_name,
-                            })
-                            .collect::<Vec<_>>(),
-                    )
+                    let am_functions =
+                        list_function_names(module.clone(), &source).unwrap_or_default();
+                    Some(am_functions)
                 }),
         );
 
@@ -150,18 +146,40 @@ fn new_parser() -> Result<Parser> {
     Ok(parser)
 }
 
-fn query_builder() -> Result<(Query, u32, u32)> {
+#[derive(Debug)]
+struct AmQuery {
+    query: Query,
+    func_name_idx: u32,
+    struct_name_idx: u32,
+    mod_name_idx: u32,
+    mod_contents_idx: u32,
+}
+
+fn query_builder() -> Result<AmQuery> {
     let query = Query::new(
         language(),
         include_str!("../runtime/queries/rust/autometrics.scm"),
     )?;
-    let idx = query
+    let func_name_idx = query
         .capture_index_for_name(FUNC_NAME_CAPTURE)
         .ok_or(AmlError::MissingFuncNameCapture)?;
-    let cap_idx = query
+    let struct_name_idx = query
         .capture_index_for_name(STRUCT_NAME_CAPTURE)
         .ok_or(AmlError::MissingFuncNameCapture)?;
-    Ok((query, idx, cap_idx))
+    let mod_name_idx = query
+        .capture_index_for_name(MOD_NAME_CAPTURE)
+        .ok_or(AmlError::MissingFuncNameCapture)?;
+    let mod_contents_idx = query
+        .capture_index_for_name(MOD_CONTENTS_CAPTURE)
+        .ok_or(AmlError::MissingFuncNameCapture)?;
+
+    Ok(AmQuery {
+        query,
+        func_name_idx,
+        struct_name_idx,
+        mod_name_idx,
+        mod_contents_idx,
+    })
 }
 
 // CLIPPY: we allow this unused function here to serve as a starting
@@ -188,15 +206,15 @@ fn known_struct_query_builder(annotated_struct_list: &HashSet<AmStruct>) -> Resu
 #[allow(dead_code)]
 fn list_struct_names(source: &str) -> Result<Vec<String>> {
     let mut parser = new_parser()?;
-    let (query, _, idx) = query_builder()?;
+    let query = query_builder()?;
     let parsed_source = parser.parse(source, None).ok_or(AmlError::Parsing)?;
 
     let mut cursor = tree_sitter::QueryCursor::new();
     cursor
-        .matches(&query, parsed_source.root_node(), source.as_bytes())
+        .matches(&query.query, parsed_source.root_node(), source.as_bytes())
         .filter_map(|capture| {
             capture
-                .nodes_for_capture_index(idx)
+                .nodes_for_capture_index(query.struct_name_idx)
                 .next()
                 .map(|node| node.utf8_text(source.as_bytes()).map(ToString::to_string))
         })
@@ -204,114 +222,148 @@ fn list_struct_names(source: &str) -> Result<Vec<String>> {
         .map_err(|_| AmlError::InvalidText)
 }
 
-fn list_function_names(source: &str) -> Result<Vec<String>> {
-    let mut parser = new_parser()?;
-    let (query, idx, _) = query_builder()?;
-    let parsed_source = parser.parse(source, None).ok_or(AmlError::Parsing)?;
+fn list_function_names(module: String, source: &str) -> Result<Vec<ExpectedAmLabel>> {
+    fn is_within_mod_item(node: Node, max_parent: Option<Node>, source: &str) -> bool {
+        let mut walk = node;
+        loop {
+            if walk.kind() == "mod_item" {
+                trace!(
+                    "Node was inside a mod.\nNode:{}\nMax Parent:{}\n",
+                    node.utf8_text(source.as_bytes())
+                        .map(ToString::to_string)
+                        .unwrap(),
+                    if let Some(node) = max_parent {
+                        node.utf8_text(source.as_bytes())
+                            .map(ToString::to_string)
+                            .unwrap()
+                    } else {
+                        source.to_string()
+                    }
+                );
+                break true;
+            }
+            if let Some(parent) = walk.parent() {
+                if max_parent.map_or(false, |max_parent| parent.id() == max_parent.id()) {
+                    break false;
+                }
 
-    let mut cursor = tree_sitter::QueryCursor::new();
-    cursor
-        .matches(&query, parsed_source.root_node(), source.as_bytes())
-        .filter_map(|capture| {
-            capture
-                .nodes_for_capture_index(idx)
-                .next()
-                .map(|node| node.utf8_text(source.as_bytes()).map(ToString::to_string))
-        })
-        .collect::<std::result::Result<Vec<_>, _>>()
-        .map_err(|_| AmlError::InvalidText)
+                walk = parent;
+                continue;
+            }
+            break false;
+        }
+    }
+
+    fn list_function_rec(
+        current_module: String,
+        node: Node,
+        query: &AmQuery,
+        source: &str,
+    ) -> Result<Vec<ExpectedAmLabel>> {
+        let mut res = Vec::new();
+        let mut cursor = tree_sitter::QueryCursor::new();
+
+        // Detect all functions directly in module scope
+        let direct_names = cursor
+            .matches(&query.query, node, source.as_bytes())
+            .filter_map(|capture| -> Option<Result<(Option<String>, String)>> {
+                let fn_node: Node = capture
+                    .nodes_for_capture_index(query.func_name_idx)
+                    .next()?;
+
+                // Ignore the matches that are within a mod_item, as the recursion will catch it later with the fully qualified module name.
+                if is_within_mod_item(fn_node, Some(node), source) {
+                    return None;
+                }
+
+                let fn_name: std::result::Result<String, std::str::Utf8Error> = fn_node
+                    .utf8_text(source.as_bytes())
+                    .map(ToString::to_string);
+                let struct_name: std::result::Result<Option<String>, std::str::Utf8Error> = capture
+                    .nodes_for_capture_index(query.struct_name_idx)
+                    .next()
+                    .map(|node| node.utf8_text(source.as_bytes()).map(ToString::to_string))
+                    .transpose();
+
+                match (struct_name, fn_name) {
+                    (Ok(m), Ok(f)) => Some(Ok((m, f))),
+                    (Err(_), _) => Some(Err(AmlError::InvalidText)),
+                    (_, Err(_)) => Some(Err(AmlError::InvalidText)),
+                }
+            })
+            .collect::<Result<Vec<(_, _)>>>()?;
+        res.extend(
+            direct_names
+                .into_iter()
+                .map(|(mod_name, fn_name)| ExpectedAmLabel {
+                    module: if let Some(inner) = mod_name {
+                        if current_module.is_empty() {
+                            inner
+                        } else {
+                            format!("{current_module}::{inner}")
+                        }
+                    } else {
+                        current_module.clone()
+                    },
+                    function: fn_name,
+                }),
+        );
+
+        // Detect all functions in submodule scope
+        for capture in cursor.matches(&query.query, node, source.as_bytes()) {
+            if let Some(mod_name_node) = capture.nodes_for_capture_index(query.mod_name_idx).next()
+            {
+                // We only want to consider module nodes that are direct children of the currently iterating node,
+                // because the recursion will cleanly look for deeply nested module declarations.
+                if mod_name_node
+                    .parent()
+                    .unwrap_or_else(|| panic!("The rust tree-sitter grammar guarantees that a mod_item:name has a mod_item as parent. {} capture is supposed to capture a mod_item:name", MOD_NAME_CAPTURE))
+                    .parent() != Some(node) {
+                    continue;
+                }
+
+                let mod_name = {
+                    if let Ok(val) = mod_name_node
+                        .utf8_text(source.as_bytes())
+                        .map(ToString::to_string)
+                    {
+                        val
+                    } else {
+                        continue;
+                    }
+                };
+
+                if let Some(contents_node) = capture
+                    .nodes_for_capture_index(query.mod_contents_idx)
+                    .next()
+                {
+                    let new_module = if current_module.is_empty() {
+                        mod_name
+                    } else {
+                        format!("{current_module}::{mod_name}")
+                    };
+                    trace!(
+                        "Recursing into mod {}\n{}\n\n\n",
+                        new_module,
+                        contents_node
+                            .utf8_text(source.as_bytes())
+                            .map(ToString::to_string)
+                            .unwrap()
+                    );
+                    let inner = list_function_rec(new_module, contents_node, query, source)?;
+                    res.extend(inner.into_iter())
+                }
+            }
+        }
+
+        Ok(res)
+    }
+
+    let mut parser = new_parser()?;
+    let query = query_builder()?;
+    let parsed_source = parser.parse(source, None).ok_or(AmlError::Parsing)?;
+    list_function_rec(module, parsed_source.root_node(), &query, source)
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn detect_single() {
-        let source = r#"
-        #[autometrics]
-        fn main() {}
-        "#;
-
-        let list = list_function_names(source).unwrap();
-
-        assert_eq!(list.len(), 1);
-        assert_eq!(list[0], "main");
-    }
-
-    #[test]
-    fn detect_impl_block() {
-        let source = r#"
-        #[autometrics]
-        struct Foo{};
-
-        impl Foo {
-            fn method_a() {}
-        }
-        "#;
-
-        let list = list_function_names(source).unwrap();
-
-        assert_eq!(list.len(), 1);
-        assert_eq!(list[0], "method_a");
-    }
-
-    #[test]
-    fn detect_struct_annotation() {
-        let source = r#"
-        #[autometrics]
-        struct Foo{};
-        "#;
-
-        let list = list_struct_names(source).unwrap();
-
-        assert_eq!(list.len(), 1);
-        assert_eq!(list[0], "Foo");
-    }
-
-    #[test]
-    fn detect_trait_impl_block() {
-        let source = r#"
-        #[autometrics]
-        struct Foo{};
-
-        impl A for Foo {
-            fn m_a() {}
-        }
-        "#;
-
-        let list = list_function_names(source).unwrap();
-
-        assert_eq!(list.len(), 1);
-        assert_eq!(list[0], "m_a");
-    }
-
-    #[test]
-    fn dodge_wrong_impl_block() {
-        let source = r#"
-        #[autometrics]
-        struct Foo{};
-
-        struct Bar{};
-
-        impl Bar {
-            fn method_one() {}
-        }
-        impl Foo {
-            fn method_two() {}
-        }
-        impl Bar {
-            fn method_three() {}
-        }
-        impl Foo {
-            fn method_four() {}
-        }
-        "#;
-
-        let list = list_function_names(source).unwrap();
-
-        assert_eq!(list.len(), 2);
-        assert!(list.contains(&"method_two".to_string()));
-        assert!(list.contains(&"method_four".to_string()));
-    }
-}
+mod tests;
